b674efb794229aa09fc4a6d42ee6cc654b38adb7
Patella <jfpatella@gmail.com>
2023-07-31 12:49:16 -0300

    Remover produtos do estoque.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Produtos novoProduto = new Produtos();
         novoProduto.quantidade = 50;
+        novoProduto.quantidade = novoProduto.quantidade - 50;
     }
 }
