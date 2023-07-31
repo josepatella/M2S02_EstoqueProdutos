28eb9c2c111dafe3677bab1facff6c660b011f73
Patella <jfpatella@gmail.com>
2023-07-31 13:00:36 -0300

    Método ToString() sobrescrito.

--- src/Entities/Produtos.java
+++ src/Entities/Produtos.java
@@ -4,4 +4,10 @@ public class Produtos {
     public String nome;
     public double preco;
     public int quantidade;
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Preço: R$" + preco + ", Quantidade: " + quantidade;
+    }
 }
+
