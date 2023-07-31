8e55081dcd28c206705f58a9237b44f1b4349993
Patella <jfpatella@gmail.com>
2023-07-31 12:51:52 -0300

    Valor total do estoque.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         Produtos novoProduto = new Produtos();
         novoProduto.quantidade = 50;
         novoProduto.quantidade = novoProduto.quantidade - 50;
+        System.out.println("Quantidade total do estoque: " + novoProduto.quantidade);
     }
 }
