8b61ddf77eb0489e17a1314f9b5c1e255601b3a8
Patella <jfpatella@gmail.com>
2023-07-31 12:35:28 -0300

    Produto Instanciado.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import Entities.Produtos;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Produtos novoProduto = new Produtos();
     }
 }
